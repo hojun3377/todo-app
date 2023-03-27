6f2d44fa0cf875efacb30508e5175589ee7fe0c9
Hojun <hojun3377@gmail.com>
2023-03-27 19:27:40 +0900

    Optimize scroll rendering using react-virtualized

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
+import { List } from 'react-virtualized';
 
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map(todo => (
+  /**
+   * react-virtualized의 List 컴포넌트에서 각 TodoItem을 렌더링 할 때 사용
+   * index, key, style 값을 파라미터에 객체타입으로 받아와서 사용한다.
+   */
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
+  return (
+    // <div className="TodoList">
+    //   {todos.map(todo => (
+    //     <TodoListItem
+    //       todo={todo}
+    //       key={todo.id}
+    //       onRemove={onRemove}
+    //       onToggle={onToggle}
+    //     />
+    //   ))}
+    // </div>
+    <List
+      className="TodoList"
+      width={512} // 전체 너비
+      height={342} // 전체 높이
+      rowCount={todos.length} // 항목 개수
+      rowHeight={57} // 항목 높이
+      rowRenderer={rowRenderer} // 항목을 렌더링할 때 쓰는 함수
+      list={todos} // 배열
+      style={{ outline: 'none' }} // List에 기본 적용되는 outline 스타일 제거
+    />
   );
 };
 
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -9,17 +9,21 @@ import {
 
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}>
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
@@ -29,6 +33,7 @@ TodoListItem.propTypes = {
   todo: PropTypes.object,
   onRemove: PropTypes.func,
   onToggle: PropTypes.func,
+  style: PropTypes.object,
 };
 
 export default React.memo(TodoListItem);
